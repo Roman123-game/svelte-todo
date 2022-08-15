9cdfa8923e7749986421960a7efa73e8d0c46ca2
RB <82284311+Roman123-game@users.noreply.github.com>
2022-08-15 17:26:37 +0300

    Update App.svelte

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,42 +1,58 @@
 <script>
   import axios from "axios";
-  import {onMount} from 'svelte'
-  $: todos = [{value:"make tea"},{value:"drink coffe"}];
-  let value = ""
-  let space = "   "
+  import { onMount } from "svelte";
+  $: todos = [];
+  let value = "";
+  let space = "   ";
 
-  onMount(async ()=> {
-    const  {data}  = await axios.get("/api/transactions");
-    todos =  data;
+  onMount(async function () {
+    try {
+      const { data } = await axios.get("/api/transactions");
+      todos = data;
+    } catch (error) {
+      console.error(error);
+    }
   });
+
   async function addTodo() {
-    const transaction = {
-      value: value
-    };
-    const response = await axios.post("/api/transactions", transaction);
-    todos = [response.data, ...todos];
-  }
-  async function removeTodo(todo) {
-    const response = await axios.delete("/api/transactions/" + todo._id);
-    if (response.data.id === todo._id) {
-     todos = todos.filter(todo => todo.id !== ev._id);
+    try {
+      const transaction = { value: value };
+      const response = await axios.post("/api/transactions", transaction);
+      todos = [response.data, ...todos];
+    } catch (error) {
+      console.log(error);
     }
   }
 
+  async function removeTodo(event) {
+    try {
+      const response = await axios.delete("/api/transactions/" + event._id);
+      const { data } = await axios.get("/api/transactions");
+      todos = data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
 </script>
 
 <main>
-  <div >
-    <input class="input is-primary" type="text" placeholder="Enter Todo" bind:value={value} />
-<p>{value}</p>
-<button class="button is-primary is-light" on:click={addTodo}>Submit</button>
-<div class="container is-flex is-flex-direction-column is-justify-content-center is-align-items-center">
-    {#each todos as todo (todo._id)}
-    <p class="is-align-self-center box ">{space}{todo.value} </p> 
-      <button class="delete" on:click={removeTodo(todo)}></button>
-    {/each}
+  <div>
+    <input
+      class="input is-primary"
+      type="text"
+      placeholder="Enter Todo"
+      bind:value
+    />
+    <p>{value}</p>
+    <button class="button is-primary is-light" on:click={addTodo}>Submit</button
+    >
+    <div
+      class="container is-flex is-flex-direction-column is-justify-content-center is-align-items-center"
+    >
+      {#each todos as todo (todo._id)}
+        <p class="is-align-self-center box ">{space}{todo.value}</p>
+        <button class="delete" on:click={removeTodo(todo)} />
+      {/each}
+    </div>
   </div>
-</div>
-
-
 </main>
