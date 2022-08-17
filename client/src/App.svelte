61aaefbf12cfffd2b8589bf5740d9a6ef8b73335
RB <82284311+Roman123-game@users.noreply.github.com>
2022-08-17 19:13:37 +0300

    update

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -36,22 +36,30 @@
 </script>
 
 <main>
-  <div>
+  <div class="mx-5">
     <input
-      class="input is-primary"
+      class="bg-green-50 border border-green-500 text-green-900 dark:text-green-400 placeholder-green-700 dark:placeholder-green-500 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-64 p-2.5 dark:bg-gray-700 dark:border-green-500 mx-auto my-2"
       type="text"
+      id="success"
       placeholder="Enter Todo"
       bind:value
     />
-    <p>{value}</p>
-    <button class="button is-primary is-light" on:click={addTodo}>Submit</button
+    <button
+      class="m-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+      on:click={addTodo}>Submit</button
     >
-    <div
-      class="container is-flex is-flex-direction-column is-justify-content-center is-align-items-center"
-    >
+    <div class="container">
       {#each todos as todo (todo._id)}
-        <p class="is-align-self-center box ">{space}{todo.value}</p>
-        <button class="delete" on:click={removeTodo(todo)} />
+        <p
+          class=" m-2 block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+        >
+        <button
+        class="text-purple-700 hover:text-white border border-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-purple-400 dark:text-purple-400 dark:hover:text-white dark:hover:bg-purple-500 dark:focus:ring-purple-900"
+        on:click={removeTodo(todo)}>X</button>
+          {todo.value}
+      
+          >
+        </p>
       {/each}
     </div>
   </div>
